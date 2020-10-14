eb10b4aac3010f80ad3fb5e95ade3d6709e3f5c7
Nicolas <contact@nicolas-devynck.fr>
2020-10-14 14:40:08 +0200

    supresion du tutoiment et compatibiliter TI

--- ohm.py
+++ ohm.py
@@ -1,31 +1,23 @@
 #setup
 from math import *
-# OS TI-83
-# from ti_system import *
-# OS Linux
-import os 
-def clr():
-    # OS TI-83
-    # disp_clr()
-    # OS Linux
-    os.system('clear')
+from ti_system import *
 # fonction watt
 def watt_ur():
-    clr()
+    disp_clr()
     print("P=U²/R")
     print("----------")
     u = int(input("U ? "))
     r = int(input("R ? "))
     return u**2/r
 def watt_ui():
-    clr()
+    disp_clr()
     print("P=U*I")
     print("----------")
     u = int(input("U ? "))
     i = int(input("I ? "))
     return u*i
 def watt_ri():
-    clr()
+    disp_clr()
     print("P=R*I²")
     print("----------")
     r = int(input("R ? "))
@@ -35,19 +27,19 @@ def watt_ri():
 def volt_pr():
     print("U=√P*R")
     print("----------")
-    clr()
+    disp_clr()
     p = int(input("P ? "))
     r = int(input("R ? "))
     return sqrt(p*r)
 def volt_pi():
-    clr()
+    disp_clr()
     print("U=P/I")
     print("----------")
     p = int(input("p ? "))
     i = int(input("I ? "))
     return p/i
 def volt_ri():
-    clr()
+    disp_clr()
     print("U=R*I")
     print("----------")
     r = int(input("R ? "))
@@ -55,21 +47,21 @@ def volt_ri():
     return r*i
 # fonction ohm
 def ohm_up():
-    clr()
+    disp_clr()
     print("R=U²/P")
     print("----------")
     u = int(input("U ? "))
     p = int(input("P ? "))
     return u**2/p
 def ohm_pi():
-    clr()
+    disp_clr()
     print("R=P/I²")
     print("----------")
     p = int(input("P ? "))
     i = int(input("I ? "))
     return p/i**2
 def ohm_ui():
-    clr()
+    disp_clr()
     print("R=U/I")
     print("----------")
     u = int(input("U ? "))
@@ -77,21 +69,21 @@ def ohm_ui():
     return u/i
 # fonction amper
 def amper_pu():
-    clr()
+    disp_clr()
     print("I=P/U")
     print("----------")
     p = int(input("P ? "))
     u = int(input("U ? "))
     return p/u
 def amper_ur():
-    clr()
+    disp_clr()
     print("I=U/R")
     print("----------")
     u = int(input("U ? "))
     r = int(input("R ? "))
     return u/r
 def amper_pr():
-    clr()
+    disp_clr()
     print("I=√P/R")
     print("----------")
     p = int(input("P ? "))
@@ -105,23 +97,23 @@ def puri():
     menu_volt = ["Exit","U=√P*R","U=P/I","U=R*I"]
     menu_ohm = ["Exit","R=U²/P","R=P/I²","R=U/I"]
     menu_amper = ["Exit","I=P/U","I=U/R","I=√P/R"]
-    clr()
+    disp_clr()
     print("Loi d'ohm")
     print("----------")
     for i in menu_0:
         print(str(menu_0.index(i))+'. '+i)
-    in_0 = input("Qu'est-ce que tu aimerais faire ? ")
+    in_0 = input("Sélectionnez votre choix ? ")
     if in_0 == "0":
-        clr()
+        disp_clr()
     elif in_0 == "1":
-        clr()
+        disp_clr()
         print("Watt = P")
         print("----------")
         for i in menu_watt:
             print(str(menu_watt.index(i))+'. '+i)
-        in_watt = input("Qu'est-ce que tu aimerais faire ? ")
+        in_watt = input("Sélectionnez votre choix ? ")
         if in_watt =="0":
-            clr()
+            disp_clr()
         elif in_watt == "1":
             print("Watt (P) = "+str(watt_ur()))
         elif in_watt == "2":
@@ -129,17 +121,17 @@ def puri():
         elif in_watt == "3":
             print("Watt (P) = "+str(watt_ri()))
         else:
-            clr()
+            disp_clr()
             print("Choix non valide") 
     elif in_0 == "2":
-        clr()
+        disp_clr()
         print("Volt = U")
         print("----------")
         for i in menu_volt:
             print(str(menu_volt.index(i))+'. '+i)
-        in_volt = input("Qu'est-ce que tu aimerais faire ? ")
+        in_volt = input("Sélectionnez votre choix ? ")
         if in_volt =="0":
-            clr()
+            disp_clr()
         elif in_volt == "1":
             print("Volt (U) = "+str(volt_pr()))
         elif in_volt == "2":
@@ -147,17 +139,17 @@ def puri():
         elif in_volt == "3":
             print("Volt (U) = "+str(volt_ri()))
         else:
-            clr()
+            disp_clr()
             print("Choix non valide") 
     elif in_0 == "3":
-        clr()
+        disp_clr()
         print("Ohm = R")
         print("----------")
         for i in menu_ohm:
             print(str(menu_ohm.index(i))+'. '+i)
-        in_ohm = input("Qu'est-ce que tu aimerais faire ? ")
+        in_ohm = input("Sélectionnez votre choix ? ")
         if in_ohm =="0":
-            clr()
+            disp_clr()
         elif in_ohm == "1":
             print("Ohm (R) = "+str(ohm_up()))
         elif in_ohm == "2":
@@ -165,17 +157,17 @@ def puri():
         elif in_ohm == "3":
             print("Ohm (R) = "+str(ohm_ui()))
         else:
-            clr()
+            disp_clr()
             print("Choix non valide") 
     elif in_0 == "4":
-        clr()
+        disp_clr()
         print("Amper = I")
         print("----------")
         for i in menu_amper:
             print(str(menu_amper.index(i))+'. '+i)
-        in_amper = input("Qu'est-ce que tu aimerais faire ? ")
+        in_amper = input("Sélectionnez votre choix ? ")
         if in_amper =="0":
-            clr()
+            disp_clr()
         elif in_amper == "1":
             print("Amper (I) = "+str(amper_pu()))
         elif in_amper == "2":
@@ -183,9 +175,9 @@ def puri():
         elif in_amper == "3":
             print("Amper (I) = "+str(amper_pr()))
         else:
-            clr()
+            disp_clr()
             print("Choix non valide") 
     else:
-        clr()
+        disp_clr()
         print("Choix non valide") 
 puri()
