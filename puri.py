ed56387a657cd211da34a9f656e3aa94f7207bd2
Devynck <info@nicolas-devynck.fr>
2023-06-06 21:22:02 +0200

    ajout de la calculatrice puri *non tester*

--- puri.py
+++ puri.py
@@ -1,4 +1,8 @@
+# import ti_system import *
+# import math import *
 import math
+# disp_clr()
+print("----------")
 print("Formule PURI")
 print("----------")
 P=input("P:Watt = ? ")
@@ -25,14 +29,93 @@ else:
     I=int(I)
 
 # verification qu'il y a au mois 2 valeur
-# todo
-if P>0 and U>0 and R>0:
-    print("Calcule imposible ")
+if P==0 and U==0 and R==0:
+    # disp_clr()
+    print("----------")
+    print("Information incorrecte")
+    print("----------")
+    print("Calcule imposible")
+    exit()
+if P==0 and U==0 and I==0:
+    # disp_clr()
+    print("----------")
+    print("Information incorrecte")
+    print("----------")
+    print("Calcule imposible")
+    exit()
+if P==0 and R==0 and I==0:
+    # disp_clr()
+    print("----------")
+    print("Information incorrecte")
+    print("----------")
+    print("Calcule imposible")
+    exit()
+if U==0 and R==0 and I==0:
+    # disp_clr()
+    print("----------")
+    print("Information incorrecte")
+    print("----------")
+    print("Calcule imposible")
+    exit()
 
-# todo calcul selont les valeur fournis
+# calcule
 if P>0 and I>0:
-    print("R = "+str(P/I**2)+" Ohm")
+    # disp_clr()
+    print("----------")
+    print("Resultat")
+    print("----------")
+    print("P = "+str(P)+" Watt")
     print("U = "+str(P/I)+" Volt")
+    print("R = "+str(P/I**2)+" Ohm")
+    print("I = "+str(I)+" Ampere")
+    exit()
 if P>0 and R>0:
-    print("I = "+str(sqrt(P/R))+" Ampere")
-    print("U = "+str(sqrt(P*R))+" Volt")
+    # disp_clr()
+    print("----------")
+    print("Resultat")
+    print("----------")
+    print("P = "+str(P)+" Watt")
+    print("U = "+str(math.sqrt(P*R))+" Volt")
+    print("R = "+str(R)+" Ohm")
+    print("I = "+str(math.sqrt(P/R))+" Ampere")
+    exit()
+if P>0 and U>0:
+    # disp_clr()
+    print("----------")
+    print("Resultat")
+    print("----------")
+    print("P = "+str(P)+" Watt")
+    print("U = "+str(U)+" Volt")
+    print("R = "+str(U**2/P)+" Ohm")
+    print("I = "+str(P/U)+" Ampere")
+    exit()
+if I>0 and R>0:
+    # disp_clr()
+    print("----------")
+    print("Resultat")
+    print("----------")
+    print("P = "+str(R*I**2)+" Watt")
+    print("U = "+str(R*I)+" Volt")
+    print("R = "+str(R)+" Ohm")
+    print("I = "+str(I)+" Ampere")
+    exit()
+if I>0 and U>0:
+    # disp_clr()
+    print("----------")
+    print("Resultat")
+    print("----------")
+    print("P = "+str(U*I)+" Watt")
+    print("U = "+str(U)+" Volt")
+    print("R = "+str(U/I)+" Ohm")
+    print("I = "+str(I)+" Ampere")
+    exit()
+if R>0 and U>0:
+    # disp_clr()
+    print("----------")
+    print("Resultat")
+    print("----------")
+    print("P = "+str(U**2*R)+" Watt")
+    print("U = "+str(U)+" Volt")
+    print("R = "+str(R)+" Ohm")
+    print("I = "+str(U/R)+" Ampere")
+    exit()
